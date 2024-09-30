58ec515abd17df12702e443d4e25e7f2a406b799
astrimbu <1097487+astrimbu@users.noreply.github.com>
2024-09-29 23:23:15 -0500

    v0.0.1 Farmer and Smiley in CharacterSelect

--- scripts/character_select.gd
+++ scripts/character_select.gd
@@ -4,61 +4,51 @@ var character = {
 	"name": "Robot",
 }
 
-@onready var character_name_label = $CanvasLayer/UI/CharacterName
 @onready var select_button = $CanvasLayer/UI/SelectButton
-@onready var character_sprite = $CanvasLayer/CharacterSprite
+@onready var character_sprite = $CanvasLayer/UI/CharacterSprite
+@onready var character_sprite2 = $CanvasLayer/UI/CharacterSprite2
+@onready var character_sprite3 = $CanvasLayer/UI/CharacterSprite3
 @onready var animation_player = $CanvasLayer/AnimationPlayer
+@onready var animation_player2 = $CanvasLayer/AnimationPlayer2
+@onready var animation_player3 = $CanvasLayer/AnimationPlayer3
 
-# Add variables for navigation
-var current_character_index = 0
-var characters = ["Robot"]  # Add more character names here when you have them
+var characters = ["Robot", "Farmer", "Smiley"]
 
 func _ready():
-	update_character_preview()
 	select_button.pressed.connect(on_select_pressed)
 	play_idle_animation()
-	character_sprite.show()
-	setup_highlight_effect()
 	
-	# Show highlight for the initial character
-	toggle_highlight_effect(true)
-
-func _input(event):
-	if event.is_action_pressed("ui_left"):
-		change_character(-1)
-	elif event.is_action_pressed("ui_right"):
-		change_character(1)
-
-func change_character(direction):
-	current_character_index = (current_character_index + direction) % characters.size()
-	if current_character_index < 0:
-		current_character_index = characters.size() - 1
-	character["name"] = characters[current_character_index]
-	update_character_preview()
-	
-	# Update highlight for the new character
-	toggle_highlight_effect(true)
-
-func update_character_preview():
-	character_name_label.text = character["name"]
-	# Here you would also update the character_sprite texture
-	# character_sprite.texture = load("res://path/to/" + character["name"].to_lower() + "_sprite.png")
+	# Initialize all sprites with no highlight
+	for sprite in [character_sprite, character_sprite2, character_sprite3]:
+		sprite.material.set_shader_parameter("outline_width", 0.0)
+		sprite.material.set_shader_parameter("outline_color", Color(1, 1, 1, 0))
 
 func play_idle_animation():
 	animation_player.play("idleto")
+	animation_player2.play("idleto-normal")
+	animation_player3.play("idleto-normal")
 
 func on_select_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	if ResourceLoader.exists("res://scenes/main.tscn"):
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/main.tscn")
 
-func setup_highlight_effect():
-	character_sprite.material.set_shader_parameter("outline_width", 0.0)
-	character_sprite.material.set_shader_parameter("outline_color", Color(1, 1, 1, 0))  # White, fully transparent
-
-func toggle_highlight_effect(highlight_on: bool):
+func toggle_highlight(sprite):
+	for s in [character_sprite, character_sprite2, character_sprite3]:
+		s.material.set_shader_parameter("outline_width", 0.0)
+		s.material.set_shader_parameter("outline_color", Color(1, 1, 1, 0))
+	
 	var tween = create_tween()
-	if highlight_on:
-		tween.tween_property(character_sprite.material, "shader_parameter/outline_width", 0.5, 0.2)
-		tween.parallel().tween_property(character_sprite.material, "shader_parameter/outline_color", Color(1, 1, 1, 1), 0.2)
-	else:
-		tween.tween_property(character_sprite.material, "shader_parameter/outline_width", 0.0, 0.2)
-		tween.parallel().tween_property(character_sprite.material, "shader_parameter/outline_color", Color(1, 1, 1, 0), 0.2)
+	tween.tween_property(sprite.material, "shader_parameter/outline_width", 1.0, 0.2)
+	tween.parallel().tween_property(sprite.material, "shader_parameter/outline_color", Color(1, 1, 1, 1), 0.2)
+
+func _on_select_button_2_pressed():
+	toggle_highlight(character_sprite2)
+	on_select_pressed()
+
+func _on_select_button_pressed():
+	toggle_highlight(character_sprite)
+	on_select_pressed()
+
+func _on_select_button_3_pressed():
+	toggle_highlight(character_sprite3)
+	on_select_pressed()
