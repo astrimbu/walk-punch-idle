edbf5f81adbf49778fcc4e191a8a8d44bffcacca
astrimbu <1097487+astrimbu@users.noreply.github.com>
2024-09-30 16:09:56 -0500

    v0.0.3 Smiley dance

--- scripts/SmileyAnimations.gd
+++ scripts/SmileyAnimations.gd
@@ -6,3 +6,7 @@ func _init(player: CharacterBody2D):
 	super._init(player)
 	animation_player = player.get_node("AnimationPlayer2")
 	sprite = player.get_node("Sprite2D2")
+
+func play_punch_animation(last_direction: Vector2):
+	_flip_sprite(last_direction.x)
+	_play_directional_animation("gangnam", last_direction)
