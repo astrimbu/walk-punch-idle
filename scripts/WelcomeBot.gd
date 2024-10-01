75f28eb43ed728d90d3b4882b4c4735f4f1e83a7
astrimbu <1097487+astrimbu@users.noreply.github.com>
2024-10-01 18:10:01 -0500

    v0.0.5 ExclamationMark

--- scripts/WelcomeBot.gd
+++ scripts/WelcomeBot.gd
@@ -1,15 +1,15 @@
 extends Node
 
+@onready var ap = $AnimationPlayer
+@onready var ap2 = $AnimationPlayer2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	ap.play("idleto")
+	ap2.play("rotate")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	# player is near WelcomeBot
